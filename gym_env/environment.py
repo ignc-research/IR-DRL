b3c59df82b3b091f7cb45e243cceb987e6f43bb7
Benno Kutschank <benku@live.de>
2023-01-09 18:15:33 +0100

    start gym env

---
+++ gym_env/environment.py
@@ -0,0 +1,22 @@
+import gym
+import numpy as np
+from robot.robot import Robot
+from sensor.sensor import Sensor
+from goal.goal import Goal
+from world.world import World
+
+class ModularDRLEnv(gym.Env):
+
+    def __init__(self, env_config):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def _reward(self):
+        pass
+
+    
