9a0e8ddfd7226d96d9fa97ba5b6a25028c266f58
Benno Kutschank <benku@live.de>
2023-01-20 18:37:27 +0100

    almost done with table experiment, rest tomorrow

--- world/obstacles/pybullet_shapes.py
+++ world/obstacles/pybullet_shapes.py
@@ -5,30 +5,30 @@ from typing import Union
 
 class Sphere(Obstacle):
 
-    def __init__(self, position: Union[list, np.ndarray], rotation: Union[list, np.ndarray], trajectory: list, velocity: float, radius: float, color: list=[0.75,0.75,0.75,1]) -> None:
-        super().__init__(position, rotation, trajectory, velocity)
+    def __init__(self, position: Union[list, np.ndarray], rotation: Union[list, np.ndarray], trajectory: list, move_step: float, radius: float, color: list=[0.75,0.75,0.75,1]) -> None:
+        super().__init__(position, rotation, trajectory, move_step)
 
         self.radius = radius
         self.color = color
 
     def build(self) -> int:
         self.object_id = pyb.createMultiBody(baseMass=0,
-                                    baseVisualShapeIndex=pyb.createVisualShape(shapeType=pyb.GEOM_SPHERE, radius=self.radius, rgbaColor=[0.75,0.75,0.75,1]),
+                                    baseVisualShapeIndex=pyb.createVisualShape(shapeType=pyb.GEOM_SPHERE, radius=self.radius, rgbaColor=self.color),
                                     baseCollisionShapeIndex=pyb.createCollisionShape(shapeType=pyb.GEOM_SPHERE, radius=self.radius),
                                     basePosition=self.position_orig)
         return self.object_id
 
 class Box(Obstacle):
 
-    def __init__(self, position: Union[list, np.ndarray], rotation: Union[list, np.ndarray], trajectory: list, velocity: float, halfExtents: Union[list, np.ndarray], color=[0.5,0.5,0.5,1]) -> None:
-        super().__init__(position, rotation, trajectory, velocity)
+    def __init__(self, position: Union[list, np.ndarray], rotation: Union[list, np.ndarray], trajectory: list, move_step: float, halfExtents: Union[list, np.ndarray], color=[0.5,0.5,0.5,1]) -> None:
+        super().__init__(position, rotation, trajectory, move_step)
 
         self.color = color
         self.halfExtents = halfExtents
 
     def build(self) -> int:
         self.object_id = pyb.createMultiBody(baseMass=0,
-                                    baseVisualShapeIndex=pyb.createVisualShape(shapeType=pyb.GEOM_BOX, halfExtents=self.halfExtents, rgbaColor=[0.5,0.5,0.5,1]),
+                                    baseVisualShapeIndex=pyb.createVisualShape(shapeType=pyb.GEOM_BOX, halfExtents=self.halfExtents, rgbaColor=self.color),
                                     baseCollisionShapeIndex=pyb.createCollisionShape(shapeType=pyb.GEOM_BOX, halfExtents=self.halfExtents),
                                     basePosition=self.position_orig)
 
--- world/world_implementations/tableexperiment.py
+++ world/world_implementations/tableexperiment.py
@@ -2,7 +2,9 @@ from world.world import World
 import numpy as np
 import pybullet as pyb
 from world.obstacles.human import Human
+from world.obstacles.pybullet_shapes import Box
 import pybullet_data as pyb_d
+from random import choice
 
 __all__ = [
     'TableExperiment'
@@ -14,8 +16,6 @@ class TableExperiment(World):
     """
 
     def __init__(self, workspace_boundaries: list, 
-                       robot_base_positions: list, 
-                       robot_base_orientations: list,
                        sim_step: float,
                        num_obstacles: int,
                        obstacle_velocities: list,
@@ -23,31 +23,67 @@ class TableExperiment(World):
                        human_positions: list,
                        human_rotations: list,
                        human_trajectories: list,
-                       ee_start_overwrite: list=[],
-                       target_overwrite: list=[]):
-        super().__init__(workspace_boundaries, robot_base_positions, robot_base_orientations, sim_step)
+                       human_reactive: list,
+                       ee_starts: list=[],
+                       targets: list=[],
+                       obstacle_positions: list=[],
+                       obstacle_trajectories: list=[]):
+        super().__init__(workspace_boundaries, sim_step)
         # INFO: if multiple robot base positions are given, we will assume that the first one is the main one for the experiment
 
         self.num_obstacles = num_obstacles
         self.num_humans = num_humans
         self.obstacle_velocities = obstacle_velocities
-        self.ee_start_overwrite = ee_start_overwrite
-        self.target_overwrite = target_overwrite
 
+        # all of the following lists serve as overwrites for env functionality
+        # useful for getting a repeatable starting point for evaluation
+        # if left as empty lists the env will generate random ones, useful for training
+        self.ee_starts = ee_starts
+        self.targets = targets
+        self.obstacle_positions = obstacle_positions
+        self.obstacle_trajectories = obstacle_trajectories
+
+        # handle human stuff
         self.humans = []
         self.human_positions = human_positions
         self.human_rotations = human_rotations
         self.human_trajectories = human_trajectories
+        self.human_reactive = human_reactive  # list of bools that determines if the human in question will raise his arm if the robot gets near enough
+        self.human_ee_was_near = [False for i in range(num_humans)]  # see update method
+        self.near_threshold = 0.5
+
+        self.obstacle_objects = []
         
     def build(self):
         # ground plate
         self.objects_ids.append(pyb.loadURDF("workspace/plane.urdf", [0, 0, -0.01]))
         # table
-        self.objects_ids.append(pyb.loadURDF(pyb_d.getDataPath()+"/table/table.urdf", useFixedBase=True, globalScaling=2))
+        self.objects_ids.append(pyb.loadURDF(pyb_d.getDataPath()+"/table/table.urdf", useFixedBase=True, globalScaling=1.75))
+        # humans
         for i in range(self.num_humans):
             human = Human(self.human_positions[i], self.human_rotations[i], self.human_trajectories[i], self.sim_step, 1.5)
             human.build()
             self.humans.append(human)
+        # obstacles
+        for i in range(self.num_obstacles):
+            # if there are no given obstacle positions, randomly generate some
+            if not self.obstacle_positions:
+                pass
+            else:
+                position = self.obstacle_positions[i]
+            # if there are no given obstacle trajectories, randomly generate some
+            if not self.obstacle_trajectories:
+                pass
+            else:
+                trajectory = self.obstacle_trajectories[i]
+            # create random dimensions for obstacles 
+            halfExtents = [0.05, 0.05, 0.05]
+            # create random velocities / move steps
+            move_step = 0
+            # create obstacles
+            obs = Box(position, [0, 0, 0, 1], trajectory, move_step, halfExtents, color=[1, 0, 0, 1])      
+            self.objects_ids.append(obs.build())
+            self.obstacle_objects.append(obs)    
 
     def reset(self):
         self.objects_ids = []
@@ -59,18 +95,28 @@ class TableExperiment(World):
         self.humans = []
 
     def update(self):
-        for human in self.humans:
+        for idx, human in enumerate(self.humans):
             human.move()
+            if self.human_reactive[idx]:
+                near = False or self.human_ee_was_near[idx]
+                if not near:  # check if end effector is near
+                    for robot in self.robots_in_world:
+                        if np.linalg.norm(robot.position_rotation_sensor.position - human.position) <= self.near_threshold:
+                            near = True
+                            self.human_ee_was_near[idx] = True
+                            break
+                if near:
+                    human.raise_hands()
 
     def create_ee_starting_points(self) -> list:
         # use the preset starting points if there are some
-        if self.ee_start_overwrite:
+        if self.ee_starts:
             ret = []
-            for idx in range(len(self.ee_start_overwrite)):
+            for idx in range(len(self.ee_starts)):
                 standard_rot = np.array([np.pi, 0, np.pi])
                 random_rot = np.random.uniform(low=-np.pi, high=np.pi, size=(3,))
                 standard_rot += random_rot * 0.1
-                ret.append((self.ee_start_overwrite[idx], np.array(pyb.getQuaternionFromEuler(standard_rot.tolist()))))
+                ret.append((self.ee_starts[idx], np.array(pyb.getQuaternionFromEuler(standard_rot.tolist()))))
                 #ret.append((self.ee_start_overwrite[idx], None))
             return ret
         # otherwise generate randomly
@@ -79,12 +125,18 @@ class TableExperiment(World):
 
     def create_position_target(self) -> list:
         # use the preset targets if there are some
-        if self.target_overwrite:
-            self.position_targets = self.target_overwrite
-            return self.target_overwrite
+        if self.targets:
+            self.position_targets = self.targets
+            return self.targets
         # otherwise generate randomly
         else:
-            pass  # TODO
+            # as this is a predetermined experiment we chose goals from a curated list
+            possible_targets = [np.array([-0.65, -0.65, 1.2]),
+                                np.array([])
+                                ]
+            target = choice(possible_targets)
+            self.position_targets = [target]
+            return [target]
 
     def create_rotation_target(self) -> list:
         return None  # not needed for now
