481b68ba84e0e586b819565d996a4f838079cec7
Benno Kutschank <benku@live.de>
2023-01-25 17:58:53 +0100

    Added Facu's improvements to original UR5 lidar class and made it more customizable

--- sensor/lidar/lidar_implementations/lidar_sensors_ur5.py
+++ sensor/lidar/lidar_implementations/lidar_sensors_ur5.py
@@ -18,8 +18,8 @@ class LidarSensorUR5(LidarSensor):
     Lidar class adapted for the use with the UR5. Features rays coming from the end effector and several wrist links.
     """
 
-    def __init__(self, normalize: bool, add_to_observation_space: bool, add_to_logging: bool, sim_step: float, update_steps: int, robot: Robot, indicator_buckets:int, ray_start: float, ray_end: float, num_rays_side: int, num_rays_circle_directions: int, render: bool = False, indicator: bool = True):
-        super().__init__(normalize, add_to_observation_space, add_to_logging, sim_step, update_steps, robot, indicator_buckets, render, )
+    def __init__(self, normalize: bool, add_to_observation_space: bool, add_to_logging: bool, sim_step: float, update_steps: int, robot: Robot, indicator_buckets:int, ray_start: float, ray_end: float, num_rays_side: int, num_rays_circle_directions: int, activated_links=[0,1,2,3,4], render: bool = False, indicator: bool = True):
+        super().__init__(normalize, add_to_observation_space, add_to_logging, sim_step, update_steps, robot, indicator_buckets, render, indicator)
 
         # lidar setup attributes
         self.ray_start = ray_start  # offset of the ray start from the mesh center
@@ -27,8 +27,30 @@ class LidarSensorUR5(LidarSensor):
         self.num_rays_circle_directions = num_rays_circle_directions  # number of directions that the circle is divided into for the sideways rays
         self.num_rays_side = num_rays_side  # rays to cast per sideways direction
 
+        # indicator conversion setup
+        raw_bucket_size = 1 / indicator_buckets  # 1 is the range of pybullet lidar data (from 0 to 1)
+        indicator_label_diff = 2 / indicator_buckets  # 2 is the range of the indicator data (from -1 to 1)
+        # lambda function to convert to indicator based on bucket size
+        self.raw_to_indicator = lambda x : 1 if x >= 0.99 else round((np.max([(np.ceil(x/raw_bucket_size)-1),0]) * indicator_label_diff - 1),5)
+        # short explanation: takes a number between 0 and 1, assigns it a bucket in the range, and returns the corresponding bucket in the range of -1 and 1
+        # the round is thrown in there to prevent weird numeric appendages that came up in testing, e.g. 0.200000000004, -0.199999999999 or the like
+
+        # set which parts of the lidar are being used
+        # usage: 
+        # 0: end effector forward ray
+        # 1: writs1 rays
+        # 2: wrist2 rays
+        # 3: wrist3 rays
+        # 4: arm3 rays
+        self.activated_links = np.zeros(5)
+        for link in activated_links:
+            self.activated_links[link] = 1
+
+        self.lidar_indicator_shape = int(self.activated_links[0] + np.sum(self.activated_links[1:]) * self.num_rays_circle_directions)
+
+
     def get_observation_space_element(self) -> dict:
-        return {self.output_name: Box(low=-1, high=1, shape=(1 + 4 * self.num_rays_circle_directions,), dtype=np.float32)}
+        return {self.output_name: Box(low=-1, high=1, shape=(self.lidar_indicator_shape,), dtype=np.float32)}
 
     def _get_lidar_data(self):
         
@@ -43,51 +65,66 @@ class LidarSensorUR5(LidarSensor):
         linkState_wrist3 = pyb.getLinkState(self.robot.object_id, 6)
         linkState_arm3 = pyb.getLinkState(self.robot.object_id, 3)
 
-        # create frame matrices
-        frame_ee = np.eye(4)
-        frame_ee[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_ee[5]), (3,3))
-        frame_ee[0:3, 3] = linkState_ee[4]
-        frame_wrist1 = np.eye(4)
-        frame_wrist1[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_wrist1[5]), (3,3))
-        frame_wrist1[0:3, 3] = linkState_wrist1[4]
-        frame_wrist2 = np.eye(4)
-        frame_wrist2[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_wrist2[5]), (3,3))
-        frame_wrist2[0:3, 3] = linkState_wrist2[4]
-        frame_wrist3 = np.eye(4)
-        frame_wrist3[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_wrist3[5]), (3,3))
-        frame_wrist3[0:3, 3] = linkState_wrist3[4]
-        frame_arm3 = np.eye(4)
-        frame_arm3[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_arm3[5]), (3,3))
-        frame_arm3[0:3, 3] = linkState_arm3[4]
+        # deal with all activated links
+        # end effector forwards ray:
+        if self.activated_links[0]:
+            frame_ee = np.eye(4)
+            frame_ee[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_ee[5]), (3,3))
+            frame_ee[0:3, 3] = linkState_ee[4]
 
-        # add the ray that goes straight forward out of the end effector
-        rays_starts.append(linkState_ee[4])
-        rays_ends.append(np.matmul(frame_ee, np.array([0, 0, self.ray_end, 1]).T)[0:3].tolist())
+            rays_starts.append(linkState_ee[4])
+            rays_ends.append(np.matmul(frame_ee, np.array([0, 0, self.ray_end, 1]).T)[0:3].tolist())
 
-        # run through each frame to add ray starts and ends
-        for angle in np.linspace(-np.pi/2, np.pi/2, self.num_rays_circle_directions):
-            for i in range(self.num_rays_side):
-                interval = 0.01
-                rays_starts.append(np.matmul(frame_wrist3, np.array([0.0, i * interval - 0.05, 0.0, 1]).T)[0:3].tolist())
-                rays_ends.append(np.matmul(frame_wrist3, np.array([self.ray_end * np.sin(angle), i * interval - 0.05, self.ray_end * np.cos(angle), 1]).T)[0:3].tolist())
-        for angle in np.linspace(-np.pi/2, np.pi/2, self.num_rays_circle_directions):
-            for i in range(self.num_rays_side):
-                # TODO: this does not seem to work for all orientations of the UR5 robot
-                # at some angles, the rays of this wrist will all point towards the inside
-                # this doesn't happen in the default experiments, but might become acute if other experiments use different poses
-                interval = 0.01
-                rays_starts.append(np.matmul(frame_wrist2, np.array([0.0, 0.0, i * interval - 0.03, 1]).T)[0:3].tolist())
-                rays_ends.append(np.matmul(frame_wrist2, np.array([-self.ray_end * np.cos(angle), self.ray_end * np.sin(angle), i * interval - 0.03, 1]).T)[0:3].tolist())
-        for angle in np.linspace(-np.pi/2, np.pi/2, self.num_rays_circle_directions):
-            for i in range(self.num_rays_side):
-                interval = 0.01
-                rays_starts.append(np.matmul(frame_wrist1, np.array([0.0, i * interval - 0.03, 0.0, 1]).T)[0:3].tolist())
-                rays_ends.append(np.matmul(frame_wrist1, np.array([self.ray_end * np.sin(angle), i * interval - 0.03, self.ray_end * np.cos(angle), 1]).T)[0:3].tolist())
-        for angle in np.linspace(-3*np.pi/4, np.pi, self.num_rays_circle_directions):
-            for i in range(self.num_rays_side):
-                interval = 0.02
-                rays_starts.append(np.matmul(frame_arm3, np.array([0.0, 0.0, i * interval + 0.1, 1]).T)[0:3].tolist())
-                rays_ends.append(np.matmul(frame_arm3, np.array([self.ray_end * np.sin(angle), -self.ray_end * np.cos(angle), i * interval + 0.1, 1]).T)[0:3].tolist())
+        # wrist 3 rays (half circle)
+        if self.activated_links[3]:
+            frame_wrist3 = np.eye(4)
+            frame_wrist3[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_wrist3[5]), (3,3))
+            frame_wrist3[0:3, 3] = linkState_wrist3[4]
+
+            for angle in np.linspace(-np.pi/2, np.pi/2, self.num_rays_circle_directions):
+                for i in range(self.num_rays_side):
+                    interval = 0.01
+                    rays_starts.append(np.matmul(frame_wrist3, np.array([0.0, i * interval - 0.05, 0.0, 1]).T)[0:3].tolist())
+                    rays_ends.append(np.matmul(frame_wrist3, np.array([self.ray_end * np.sin(angle), i * interval - 0.05, self.ray_end * np.cos(angle), 1]).T)[0:3].tolist())
+        
+        # wrist 2 rays (half circle)
+        if self.activated_links[2]:
+            frame_wrist2 = np.eye(4)
+            frame_wrist2[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_wrist2[5]), (3,3))
+            frame_wrist2[0:3, 3] = linkState_wrist2[4]
+
+            for angle in np.linspace(-np.pi/2, np.pi/2, self.num_rays_circle_directions):
+                for i in range(self.num_rays_side):
+                    # TODO: this does not seem to work for all orientations of the UR5 robot
+                    # at some angles, the rays of this wrist will all point towards the inside
+                    # this doesn't happen in the default experiments, but might become acute if other experiments use different poses
+                    interval = 0.01
+                    rays_starts.append(np.matmul(frame_wrist2, np.array([0.0, 0.0, i * interval - 0.03, 1]).T)[0:3].tolist())
+                    rays_ends.append(np.matmul(frame_wrist2, np.array([-self.ray_end * np.cos(angle), self.ray_end * np.sin(angle), i * interval - 0.03, 1]).T)[0:3].tolist())
+
+        # wrist 1 rays (half circle)
+        if self.activated_links[1]:
+            frame_wrist1 = np.eye(4)
+            frame_wrist1[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_wrist1[5]), (3,3))
+            frame_wrist1[0:3, 3] = linkState_wrist1[4]
+
+            for angle in np.linspace(-np.pi/2, np.pi/2, self.num_rays_circle_directions):
+                for i in range(self.num_rays_side):
+                    interval = 0.01
+                    rays_starts.append(np.matmul(frame_wrist1, np.array([0.0, i * interval - 0.03, 0.0, 1]).T)[0:3].tolist())
+                    rays_ends.append(np.matmul(frame_wrist1, np.array([self.ray_end * np.sin(angle), i * interval - 0.03, self.ray_end * np.cos(angle), 1]).T)[0:3].tolist())
+
+        # arm 3 rays (full circle)
+        if self.activated_links[4]:
+            frame_arm3 = np.eye(4)
+            frame_arm3[:3, :3] = np.reshape(pyb.getMatrixFromQuaternion(linkState_arm3[5]), (3,3))
+            frame_arm3[0:3, 3] = linkState_arm3[4]
+
+            for angle in np.linspace(-3*np.pi/4, np.pi, self.num_rays_circle_directions):
+                for i in range(self.num_rays_side):
+                    interval = 0.02
+                    rays_starts.append(np.matmul(frame_arm3, np.array([0.0, 0.0, i * interval + 0.1, 1]).T)[0:3].tolist())
+                    rays_ends.append(np.matmul(frame_arm3, np.array([self.ray_end * np.sin(angle), -self.ray_end * np.cos(angle), i * interval + 0.1, 1]).T)[0:3].tolist())
 
         results = pyb.rayTestBatch(rays_starts, rays_ends)
         
@@ -106,43 +143,22 @@ class LidarSensorUR5(LidarSensor):
         return np.array(results, dtype=object)[:,2]  # keeps only the distance information
 
     def _process_raw_lidar(self, raw_lidar_data):
-        raw_bucket_size = 1 / self.indicator_buckets  # 1 is the range of pybullet lidar data (from 0 to 1)
-        indicator_label_diff = 2 / self.indicator_buckets  # 2 is the range of the indicator data (from -1 to 1)
-        
-        # lambda function to convert to indicator based on bucket size
-        raw_to_indicator = lambda x : 1 if x >= 0.99 else round((np.max([(np.ceil(x/raw_bucket_size)-1),0]) * indicator_label_diff - 1),5)
-        # short explanation: takes a number between 0 and 1, assigns it a bucket in the range, and returns the corresponding bucket in the range of -1 and 1
-        # the round is thrown in there to prevent weird numeric appendages that came up in testing, e.g. 0.200000000004, -0.199999999999 or the like
 
-        lidar_shape = 1 + 4 * self.num_rays_circle_directions 
-        indicator = np.zeros(lidar_shape)
-        distances = np.zeros(lidar_shape)
+        indicator = np.zeros(self.lidar_indicator_shape)
+        distances = np.zeros(self.lidar_indicator_shape)
 
         # conversion
         # the list slicing here is messy but basically just follows the way the rays were put into the source array following the _get_lidar_data method
         # tip
-        indicator[0] = raw_to_indicator(raw_lidar_data[0])
-        distances[0] = raw_lidar_data[0] * (self.ray_end - self.ray_start) + self.ray_start
+        if self.activated_links[0]:
+            indicator[0] = self.raw_to_indicator(raw_lidar_data[0])
+            distances[0] = raw_lidar_data[0] * (self.ray_end - self.ray_start) + self.ray_start
         # wrist 3
-        for i in range(self.num_rays_circle_directions):
-            lidar_min = raw_lidar_data[1 + i * self.num_rays_side : 1 + (i + 1) * self.num_rays_side].min()
-            indicator[1 + i] = raw_to_indicator(lidar_min)
-            distances[1 + i] = lidar_min * (self.ray_end - self.ray_start) + self.ray_start
-        # wrist 2
-        for i in range(self.num_rays_circle_directions):
-            lidar_min = raw_lidar_data[1 + self.num_rays_circle_directions * self.num_rays_side + i * self.num_rays_side : 1 + self.num_rays_circle_directions * self.num_rays_side + (i + 1) * self.num_rays_side].min()
-            indicator[1 + self.num_rays_circle_directions + i] = raw_to_indicator(lidar_min)
-            distances[1 + self.num_rays_circle_directions + i] = lidar_min * (self.ray_end - self.ray_start) + self.ray_start
-        # wrist 1
-        for i in range(self.num_rays_circle_directions):
-            lidar_min = raw_lidar_data[1 + 2 * self.num_rays_circle_directions * self.num_rays_side + i * self.num_rays_side : 1 + 2 * self.num_rays_circle_directions * self.num_rays_side + (i + 1) * self.num_rays_side].min()
-            indicator[1 + 2 * self.num_rays_circle_directions + i] = raw_to_indicator(lidar_min)
-            distances[1 + 2 * self.num_rays_circle_directions + i] = lidar_min * (self.ray_end - self.ray_start) + self.ray_start
-        # arm 3
-        for i in range(self.num_rays_circle_directions):
-            lidar_min = raw_lidar_data[1 + 3 * self.num_rays_circle_directions * self.num_rays_side + i * self.num_rays_side : 1 + 3 * self.num_rays_circle_directions * self.num_rays_side + (i + 1) * self.num_rays_side].min()
-            indicator[1 + 3 * self.num_rays_circle_directions + i] = raw_to_indicator(lidar_min)
-            distances[1 + 3 * self.num_rays_circle_directions + i] = lidar_min * (self.ray_end - self.ray_start) + self.ray_start
+        for j in range(int(np.sum(self.activated_links[1:]))): # wrist 3, wrist 2, wrist 1, arm 3
+            for i in range(self.num_rays_circle_directions):
+                lidar_min = raw_lidar_data[1 + j * self.num_rays_circle_directions * self.num_rays_side + i * self.num_rays_side : 1 + j * self.num_rays_circle_directions * self.num_rays_side + (i + 1) * self.num_rays_side].min()
+                indicator[1 + j * self.num_rays_circle_directions + i] = self.raw_to_indicator(lidar_min)
+                distances[1 + j * self.num_rays_circle_directions + i] = lidar_min * (self.ray_end - self.ray_start) + self.ray_start
     
         return indicator, distances
 
