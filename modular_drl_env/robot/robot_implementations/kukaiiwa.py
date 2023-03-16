237c5dc6ce8ba17b5f886a8c85cdb00a8bd5e1f9
Benno Kutschank <benku@live.de>
2023-03-16 08:50:12 +0100

    Made a few parameters optional for robot

--- modular_drl_env/robot/robot_implementations/kukaiiwa.py
+++ modular_drl_env/robot/robot_implementations/kukaiiwa.py
@@ -12,9 +12,9 @@ class Kukaiiwa(Robot):
 
     def __init__(self, name: str, id_num: int, world, sim_step: float, use_physics_sim: bool,
                  base_position: Union[list, np.ndarray], base_orientation: Union[list, np.ndarray],
-                 resting_angles: Union[list, np.ndarray], control_mode: int, xyz_delta: float, rpy_delta: float):
+                 resting_angles: Union[list, np.ndarray], control_mode: int, xyz_delta: float=0.005, rpy_delta: float=0.005, joint_vel_mul: float=1):
         super().__init__(name, id_num, world, sim_step, use_physics_sim, base_position, base_orientation,
-                         resting_angles, control_mode, xyz_delta, rpy_delta)
+                         resting_angles, control_mode, xyz_delta, rpy_delta, joint_vel_mul)
         # from urdf file
         self.joints_limits_lower = np.deg2rad(np.array([-170, -120, -170, -120, -170, -120, -175]))
         self.joints_limits_upper = np.deg2rad(np.array([170, 120, 170, 120, 170, 120, 175]))
